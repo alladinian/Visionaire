421a093a2c726e2cadff54a4cb07a661384fa47e
Vasilis Akoinoglou <alladinian@gmail.com>
2023-07-05 13:12:35 +0300

    More work on observation drawing

--- Sources/Visionaire/SwiftUI+Extensions.swift
+++ Sources/Visionaire/SwiftUI+Extensions.swift
@@ -9,7 +9,21 @@ import SwiftUI
 import Vision
 
 public extension View {
-
+    func drawObservations(_ observations: [VNDetectedObjectObservation], isFlipped: Bool = true, _ drawingClosure: @escaping (CGRect) -> some View) -> some View {
+        overlay(
+            GeometryReader { reader in
+                ForEach(observations, id: \.self) { observation in
+                    let denormalizedRect = VNImageRectForNormalizedRect(observation.boundingBox, Int(reader.size.width), Int(reader.size.height))
+                    drawingClosure(denormalizedRect)
+                        .frame(width: denormalizedRect.size.width,
+                               height: denormalizedRect.size.height)
+                        .offset(x: denormalizedRect.origin.x,
+                                y: denormalizedRect.origin.y)
+                }
+            }
+            .scaleEffect(x: 1, y: isFlipped ? -1 : 1)
+        )
+    }
 }
 
 public extension [VNDetectedObjectObservation] {
